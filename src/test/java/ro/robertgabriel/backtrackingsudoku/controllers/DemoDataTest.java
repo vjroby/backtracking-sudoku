6f5f506db61930bae8cb731a77fa41f3a5d7504f
Robert  Dinu <vjroby@gmail.com>
2018-06-28 08:06:48 +0700

    Added jupiter annotation.

--- src/test/java/ro/robertgabriel/backtrackingsudoku/controllers/DemoDataTest.java
+++ src/test/java/ro/robertgabriel/backtrackingsudoku/controllers/DemoDataTest.java
@@ -1,6 +1,6 @@
 package ro.robertgabriel.backtrackingsudoku.controllers;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ro.robertgabriel.backtrackingsudoku.algo.SolveBacktracking;
